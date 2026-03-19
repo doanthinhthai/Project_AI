f020a4075aa61be2ebd13be0cc06a47bcc922170
Thai Doan Thinh <thinhdt.qt@gmail.com>
2026-03-19 21:34:27 +0700

    sua doi cho cho giong code

--- game/moves.py
+++ game/moves.py
@@ -1,8 +1,4 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from Const.Values import *
+from core.constants import EMPTY
 
 class Move:
     def __init__(self, start_pos, end_pos, board):
@@ -12,9 +8,14 @@ class Move:
         self.end_col = end_pos[1]
 
         self.piece_moved = board[self.start_row][self.start_col]
-        
         self.piece_captured = board[self.end_row][self.end_col]
-        self.move_id = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
+
+        self.move_id = (
+            self.start_row * 1000
+            + self.start_col * 100
+            + self.end_row * 10
+            + self.end_col
+        )
 
     def __eq__(self, other):
         if isinstance(other, Move):
@@ -22,11 +23,12 @@ class Move:
         return False
 
     def get_chess_notation(self):
-        return f"{self.piece_moved}{self.start_col}{self.start_row} to {self.end_col}{self.end_row}"
+        return f"{self.piece_moved}: ({self.start_row}, {self.start_col}) -> ({self.end_row}, {self.end_col})"
 
     def is_capture(self):
         return self.piece_captured != EMPTY
 
     def __str__(self):
-        capture_str = f" ăn {self.piece_captured}" if self.is_capture() else " di chuyển"
-        return f"[{self.piece_moved}] từ ({self.start_row},{self.start_col}) đến ({self.end_row},{self.end_col}){capture_str}"
+        if self.is_capture():
+            return f"[{self.piece_moved}] từ ({self.start_row}, {self.start_col}) đến ({self.end_row}, {self.end_col}) ăn {self.piece_captured}"
+        return f"[{self.piece_moved}] từ ({self.start_row}, {self.start_col}) đến ({self.end_row}, {self.end_col})"
