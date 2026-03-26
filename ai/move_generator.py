cc3c5853349e04054be1bf3a49f7827376ef101e
Thai Doan Thinh <thinhdt.qt@gmail.com>
2026-03-26 21:09:57 +0700

    fix: move_generator to same

--- ai/move_generator.py
+++ ai/move_generator.py
@@ -5,11 +5,11 @@ class MoveGenerator:
     def __init__(self, board, rules, game_manager=None):
         self.board = board
         self.rules = rules
-        self.game_manager = game_manager  # dùng tạm nếu cần check
+        self.game_manager = game_manager
 
     def get_all_moves(self, color):
         """
-        Trả về tất cả nước đi hợp lệ (legal moves) của một bên
+        Trả về tất cả nước đi hợp lệ (legal moves) của một bên.
         """
         moves = []
 
@@ -25,7 +25,7 @@ class MoveGenerator:
                 for move in candidate_moves:
                     self.board.make_move(move)
 
-                    #Lọc nước đi không hợp lệ
+                    # Lọc nước đi không hợp lệ
                     if not self.is_illegal_move(color):
                         moves.append(move)
 
@@ -37,15 +37,12 @@ class MoveGenerator:
         """
         Kiểm tra nước đi có vi phạm luật không:
         - tự chiếu
-        - 2 tướng đối mặt
+        - hai tướng đối mặt
         """
-        # Nếu có game_manager thì tận dụng luôn
-        if self.game_manager:
+        if self.game_manager is not None:
             if self.game_manager.is_in_check(color):
                 return True
             if self.game_manager.kings_face_each_other():
                 return True
-            return False
 
-        # Nếu không có, fallback (sau này nên chuyển hết vào rules)
         return False
